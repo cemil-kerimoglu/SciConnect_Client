681b2c8221518428fa184da59c240549def7bcd2
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2022-10-06 13:10:36 +0200

    changed the host to my local host

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 // const API = axios.create({ baseURL: 'https://sciconnect-server.onrender.com' });
-const API = axios.create({ baseURL: 'https://sciconnectserver.onrender.com' });
+// const API = axios.create({ baseURL: 'https://sciconnectserver.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
